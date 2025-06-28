9baf71d8933019b5dd16d088a0fcd95c4cc96d96
Asit-14 <asitshakya789@gmail.com>
2025-06-28 10:21:07 +0530

    Refactor stats section and enhance styling in styles.css; add animation functionality in script.js

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-document.querySelectorAll('.learn-more-btn').forEach(button => {
-	button.addEventListener('click', () => {
-		alert('Learn more about this feature!');
-	});
-});
+
+	const statsElement = document.querySelector('.stats');
+
+  statsElement.addEventListener('animationend', () => {
+		// Restart after delay
+		setTimeout(() => {
+			statsElement.style.animation = 'none'; // Remove animation
+			void statsElement.offsetWidth;         // Trigger reflow
+			statsElement.style.animation = 'scrollStats 20s linear 1'; // Reapply
+		}, 1000); // 1 second pause before restarting
+  });
+
